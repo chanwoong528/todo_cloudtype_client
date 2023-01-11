6af6f358cbf49d2b26e2ad400d2efa5d4e883255
문찬웅 <mooncw@hankookilbo.com>
2023-01-11 15:47:42 +0900

    server domain changed

--- src/apis/_commonApi.js
+++ src/apis/_commonApi.js
@@ -1,3 +1,7 @@
-export const BASE_URL = "https://port-0-todo-cloudtype-4fuvwk25lcptzb1n.gksl2.cloudtype.app"
+// export const BASE_URL = "https://port-0-todo-cloudtype-4fuvwk25lcptzb1n.gksl2.cloudtype.app"
 
 // export const BASE_URL = "http://localhost:5000"
+
+
+
+export const BASE_URL = "https://todocloudtype-production.up.railway.app"
